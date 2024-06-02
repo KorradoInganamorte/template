b9d48b1d0a493e327a81acfd9c4980b356bc977c
Korrado Inganamorte <petristomin1812@gmail.com>
2024-06-02 12:40:18 +0300

    update || upgrade

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 
-import { montserratRegular } from '@/public/fonts'
-
 import './globals.css'
 
 const montserratGoogle = Montserrat({
@@ -22,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='leading-normal'>
-      <body className={`${montserratRegular} ${montserratGoogle.variable}`}>{children}</body>
+    <html lang="en" className='phone-sm:text-[46.25%] verticalphone:text-[47%] horizontalphone:text-[48.5%] tablet:text-[50%] laptop-sm:text-[56.25%] laptop:text-[62.5%] monitor:text-[70.5%] screen:text-[87.5%]'>
+      <body className={montserratGoogle.className}>{children}</body>
     </html>
   )
 }
